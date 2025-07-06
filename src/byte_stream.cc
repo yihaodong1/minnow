3174e3b969098071edf56259332d3cc312599262
yihaodong1 <2605279752@qq.com>
2025-07-06 19:15:11 +0800

    pass check0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,51 +6,66 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  uint64_t a = available_capacity();
+  if(a > data.size()){
+    buffer+=data;
+    bytes_pushed_ += data.size();
+  }else{
+    buffer+=data.substr(0, a);
+    bytes_pushed_ += a;
+  }
 }
 
 void Writer::close()
 {
   // Your code here.
+  close_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return close_; // Your code here.
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer.size(); // Your code here.
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_; // Your code here.
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return string_view(buffer); // Your code here.
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  uint64_t buffered = bytes_buffered();
+  if(buffered > len){
+    buffer = buffer.substr(len);
+    bytes_popped_ += len;
+  }else{
+    buffer.erase();
+    bytes_popped_ += buffered;
+  }
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return  close_ && bytes_buffered() == 0; // Your code here.
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer.size(); // Your code here.
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_; // Your code here.
 }
 
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <queue>
 
 class Reader;
 class Writer;
@@ -24,7 +25,11 @@ public:
 protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
+  uint64_t bytes_pushed_ = 0;
+  uint64_t bytes_popped_ = 0;
   bool error_ {};
+  bool close_ {};
+  std::string buffer {};
 };
 
 class Writer : public ByteStream
