65bc9cdba621f91e5691617f7d641c72832bcdf2
yihaodong1 <2605279752@qq.com>
2025-07-12 17:06:04 +0800

    format and comment

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -18,59 +18,67 @@ uint64_t TCPSender::consecutive_retransmissions() const
   return transmissions_;
 }
 
-TCPSenderMessage TCPSender::construct_msg(uint16_t window_size, bool &flag){
+TCPSenderMessage TCPSender::construct_msg( uint16_t window_size, bool& flag )
+{
   TCPSenderMessage msg;
   msg.seqno = isn_ + bytes_send_;
   msg.RST = input_.has_error();
   msg.SYN = bytes_send_ == 0;
 
-  flag |= (msg.SYN && (window_size > bytes_fly_));
+  // use flag to decide whether send msg
+  // When window_size_ > bytes_fly_, there is capability for SYN
+  flag |= ( msg.SYN && ( window_size > bytes_fly_ ) );
 
-  uint64_t len = window_size > bytes_fly_? 
-    min((size_t)window_size - bytes_fly_, TCPConfig::MAX_PAYLOAD_SIZE) 
-    - msg.SYN:0;
-  if(reader().bytes_buffered() && len > 0){
+  // Calculate the length of payload
+  uint64_t len
+    = window_size > bytes_fly_ ? min( (size_t)window_size - bytes_fly_, TCPConfig::MAX_PAYLOAD_SIZE ) - msg.SYN : 0;
+  if ( reader().bytes_buffered() && len > 0 ) {
+    // When there are payload, should send msg
     flag |= 1;
-    msg.payload = reader().peek().substr(0, len);
-    reader().pop(len);
+    msg.payload = reader().peek().substr( 0, len );
+    reader().pop( len );
   }
 
-  if(reader().is_finished() 
-      && window_size > bytes_fly_ + msg.sequence_length()){
+  if ( reader().is_finished()
+       // as FIN also occupy space
+       && window_size > bytes_fly_ + msg.sequence_length() ) {
     msg.FIN = true;
     flag |= 1;
   }
   return msg;
-
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
   // debug( "unimplemented push() called" );
-  while(window_size_ > bytes_fly_){
+  while ( window_size_ > bytes_fly_ ) {
 
     bool flag = false;
-    TCPSenderMessage msg = construct_msg(window_size_, flag);
-    if(flag && !fin_){
+    TCPSenderMessage msg = construct_msg( window_size_, flag );
+    if ( flag && !fin_ ) {
+      // use fin_ to record the send has finished, avoiding
+      // sending FIN repeatly
       fin_ = msg.FIN;
       bytes_send_ += msg.sequence_length();
       bytes_fly_ += msg.sequence_length();
-      queue_.push_back(msg);
-      transmit(msg);
+      queue_.push_back( msg );
+      transmit( msg );
+      // Start the timer
       timer_start_ = true;
-    }else {
+    } else {
       break;
     }
   }
-  if(window_size_ == 0){
+  // Special case for window_size_ == 0
+  if ( window_size_ == 0 ) {
     bool flag = false;
-    TCPSenderMessage msg = construct_msg(1, flag);
-    if(flag && !fin_){
+    TCPSenderMessage msg = construct_msg( 1, flag );
+    if ( flag && !fin_ ) {
       fin_ = msg.FIN;
       bytes_send_ += msg.sequence_length();
       bytes_fly_ += msg.sequence_length();
-      queue_.push_back(msg);
-      transmit(msg);
+      queue_.push_back( msg );
+      transmit( msg );
       timer_start_ = true;
     }
   }
@@ -88,22 +96,28 @@ TCPSenderMessage TCPSender::make_empty_message() const
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   // debug( "receive() called" );
-  if((msg.ackno.has_value()&&
-    msg.ackno.value().unwrap(isn_, bytes_acked_) <= bytes_send_)
-    || !msg.ackno.has_value()){
-  // if(msg.ackno <= isn_ + bytes_send_){
-    if(msg.RST)input_.set_error();
+  // When msg.ackno.has_value(), the value should be corret
+  // , no greater than bytes_send_
+  // when !msg.ackno.has_value(), also need to set window_size_
+  if ( ( msg.ackno.has_value() && msg.ackno.value().unwrap( isn_, bytes_acked_ ) <= bytes_send_ )
+       || !msg.ackno.has_value() ) {
+    // if(msg.ackno <= isn_ + bytes_send_){
+    if ( msg.RST )
+      input_.set_error();
     window_size_ = msg.window_size;
-    while(!queue_.empty()){
+    while ( !queue_.empty() ) {
+      // find the msg qualified
       auto it = queue_.front();
-      if(msg.ackno.has_value()&&
-          it.seqno.unwrap(isn_, bytes_acked_) + it.sequence_length() > msg.ackno.value().unwrap(isn_, bytes_acked_)){
+      if ( msg.ackno.has_value()
+           && it.seqno.unwrap( isn_, bytes_acked_ ) + it.sequence_length()
+                > msg.ackno.value().unwrap( isn_, bytes_acked_ ) ) {
         break;
       }
       bytes_fly_ -= it.sequence_length();
       bytes_acked_ += it.sequence_length();
       queue_.pop_front();
 
+      // if there are no bytes_fly_, no need to start timer
       timer_start_ = bytes_fly_ != 0;
       RTO_ms_ = initial_RTO_ms_;
       transmissions_ = 0;
@@ -115,14 +129,15 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
   // debug( "unimplemented tick({}, ...) called", ms_since_last_tick );
-  if(timer_start_)
+  if ( timer_start_ )
     current_time_ += ms_since_last_tick;
-  if(current_time_  >= RTO_ms_){
-    if(!queue_.empty()){
+  if ( current_time_ >= RTO_ms_ ) {
+    if ( !queue_.empty() ) {
+      // find the earliest msg to retransmit
       auto it = queue_.front();
-      transmit(it);
+      transmit( it );
     }
-    if(window_size_){
+    if ( window_size_ ) {
       RTO_ms_ = RTO_ms_ << 1;
       ++transmissions_;
     }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,15 +4,15 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
-#include <functional>
 #include <deque>
+#include <functional>
 
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), RTO_ms_(initial_RTO_ms)
+    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), RTO_ms_( initial_RTO_ms )
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -53,5 +53,5 @@ private:
   uint64_t RTO_ms_ {};
   uint64_t transmissions_ {};
   bool timer_start_ = false;
-  TCPSenderMessage construct_msg(uint16_t window_size, bool &flag);
+  TCPSenderMessage construct_msg( uint16_t window_size, bool& flag );
 };
